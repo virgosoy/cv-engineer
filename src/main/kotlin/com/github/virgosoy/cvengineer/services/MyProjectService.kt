1946c1b4f96aa6f600092733ca37d03c5e0f6d9a
GitHub Action <action@github.com>
2022-04-19 03:51:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/virgosoy/cvengineer/services/MyProjectService.kt
+++ src/main/kotlin/com/github/virgosoy/cvengineer/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.virgosoy.cvengineer.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.virgosoy.cvengineer.MyBundle
 
 class MyProjectService(project: Project) {
 
